b29522cddb0fd712b6ff77b54d61d29da5b1dc4d
unknown <tomerpacific@gmail.com>
2021-02-05 20:48:44 +0200

    julOnSaleReact adding react-router package

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import changeTitleWhenUserMovesAway from './titleChanger';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
