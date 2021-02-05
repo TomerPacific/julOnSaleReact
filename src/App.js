266caa05e40e0b4103acbf896f6330246c5cca57
unknown <tomerpacific@gmail.com>
2021-02-05 14:09:42 +0200

    julOnSaleReact moving Categories logic into App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import DateHeader from './DateHeader';
 import Spinner from './Spinner';
-import Categories from './Categories';
 import './App.css';
+
+const endpoint = "https://jul-on-sale.herokuapp.com/jul";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {shouldShowSpinner: true}
-    this.spinnerHandler = this.spinnerHandler.bind(this);
+    this.state = {shouldShowSpinner: true, categories: []}
   }
-  spinnerHandler() {
-    this.setState({
-      ...this.state, shouldShowSpinner: false
+  componentDidMount() {
+    if (this.state.categories.length === 0) {
+        this.fetchData();
+    } else {
+        this.buildCategories();
+    }  
+  }
+  fetchData() {
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(result => {
+        this.setState({
+            ...this.state, shouldShowSpinner: false, categories: result.message
+        });
+        if (this.props) {
+            this.props.spinnerHandler();
+        }
+        this.buildCategories(result.message);
     })
+    .catch(error => {
+        this.setState({
+            ...this.state, shouldShowSpinner: false
+        });
+    })
+  } 
+  buildCategories(categories) {
+    const mainDiv = document.querySelector('.categories');
+    for (let index = 0; index < categories.length; index++)
+    {
+        let category = categories[index];
+        let div = document.createElement('div');
+        let header = document.createElement('h3');
+        let anchor = document.createElement('a');
+        let image = document.createElement('img');
+
+        header.innerHTML = category.name;
+        image.src = 'https://github.com/TomerPacific/julOnSale/blob/master/assets/' + category.image + '.png?raw=true';
+        image.setAttribute('class', 'category');
+        anchor.href = "https://tomerpacific.github.io/julOnSale/categories/" + category.image + "/" + category.image + ".html";
+        anchor.innerHTML = '<img src=' + image.src + ' class="category" alt=' + category.name + ' title=' + category.name + '>';
+
+        div.appendChild(header);
+        div.appendChild(anchor);
+        div.setAttribute('class', 'container');
+        mainDiv.appendChild(div);
+    }
   }
   render() {
     return <div className="App">
@@ -22,7 +64,7 @@ class App extends React.Component {
     </header>
     <DateHeader />
     <Spinner status = {this.state.shouldShowSpinner}/>
-    <Categories spinnerHandler = {this.spinnerHandler}/>
+    <div className="categories"></div>
   </div>
   }
 }
