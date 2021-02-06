5ee9ac49672a807d8ee06f4032081bf46020ceb2
markgrichanik <markgrichanik@gmail.com>
2021-02-06 15:17:04 +0200

    Update Category.js

--- src/Category.js
+++ src/Category.js
@@ -1,73 +1,64 @@
-import React from 'react';
-import DateHeader from './DateHeader';
-import Spinner from './Spinner';
-
+import React from "react";
+import Spinner from "./Spinner";
+import { withRouter } from "react-router";
+import "./App.css";
+import "./Category.css";
 const endpoint = "https://jul-on-sale.herokuapp.com/category/";
 
 class Category extends React.Component {
-    constructor() {
-        super();
-        this.state = {shouldShowSpinner: true, items: []}
+  state = {
+    shouldShowSpinner: true,
+    items: [],
+    hasMore: true
+  };
+  componentDidMount() {
+    if (this.state.items.length === 0) {
+      this.fetchData();
     }
-    componentDidMount() {
-        if (this.state.items.length === 0) {
-            this.fetchData();
-        } else {
-            this.buildCategories(this.state.items);
-        }  
-      }
-      fetchData() {
-        fetch(endpoint + this.props.categoryName)
-        .then(response => response.json())
-        .then(result => {
-            this.setState({
-                ...this.state, shouldShowSpinner: false, items: result.message
-            });
-            this.buildItemsForCategory(result.message);
-        })
-        .catch(error => {
-            this.setState({
-                ...this.state, shouldShowSpinner: false
-            });
-        })
-      }
-    buildItemsForCategory(items) {
-        const mainDiv = document.querySelector('#mainDiv');
-        for (let index = 0; index < items.length; index++)
-	    {  
-            let item = items[index];
-            let div = document.createElement('div');
-            let nameHeader = document.createElement('h3');
-            let priceHeader = document.createElement('h2');
-            let anchor = document.createElement('a');
-            let image = document.createElement('img');
+  }
+  fetchData() {
+    fetch(endpoint + this.props.match.params.categoryName)
+      .then(response => response.json())
+      .then(result => {
+        this.setState({
+          ...this.state,
+          shouldShowSpinner: false,
+          items: result.message
+        });
+      })
+      .catch(error => {
+        this.setState({
+          ...this.state,
+          shouldShowSpinner: false
+        });
+      });
+  }
 
-            nameHeader.innerHTML = item.name;
-            image.src = item.image;
-            anchor.href = item.link;
-            priceHeader.innerHTML = item.price;
-
-            image.setAttribute('class', 'item');
-            anchor.innerHTML = '<img src=' + image.src + ' class="item" alt=' + item.name + ' title=' + item.name + '>';
-
-            div.appendChild(nameHeader);
-            div.appendChild(anchor);
-            div.appendChild(priceHeader);
-            div.setAttribute('class', 'container');
-            mainDiv.appendChild(div);
-	    }
-    }
-    render() {
-        return <div>
-            <header className="App-header">
-            What's On Sale @ Jul {this.props.categoryName}
-            </header>
-            <DateHeader />
-            <Spinner status = {this.state.shouldShowSpinner}/>
-            <div id="mainDiv"></div>
+  render() {
+    return (
+      <>
+        <Spinner status={this.state.shouldShowSpinner}/>
+        <div className={"categoryItems"}>
+          {this.state.items.map(item => {
+            return (
+              <div key={`categoryItem-${item.link}`} className={"container"}>
+                <h3>{item.name}</h3>
+                <a href={item.link}>
+                  <img
+                    src={item.image}
+                    className={"item"}
+                    alt={item.name}
+                    title={item.name}
+                  />
+                </a>
+                <h2>{item.price}</h2>
+              </div>
+            );
+          })}
         </div>
-    }
+      </>
+    );
+  }
 }
 
-
-export default Category;
+export default withRouter(Category);
