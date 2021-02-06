e01d47c7ef361326f053c0bd57077d8985471972
markgrichanik <markgrichanik@gmail.com>
2021-02-06 15:16:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,73 +1,47 @@
-import React from 'react';
-import DateHeader from './DateHeader';
-import Spinner from './Spinner';
-import './App.css';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import Category from './Category';
+import React from "react";
+import DateHeader from "./DateHeader";
+import Spinner from "./Spinner";
+import "./App.css";
+import Routes from "./Routes";
 
 const endpoint = "https://jul-on-sale.herokuapp.com/jul";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {shouldShowSpinner: true, categories: []}
+  constructor(props) {
+    super(props);
+    this.state = { shouldShowSpinner: true, categories: [] };
   }
   componentDidMount() {
     if (this.state.categories.length === 0) {
-        this.fetchData();
-    } else {
-        this.buildCategories(this.state.categories);
-    }  
+      this.fetchData();
+    }
   }
   fetchData() {
     fetch(endpoint)
-    .then(response => response.json())
-    .then(result => {
+      .then(response => response.json())
+      .then(result => {
         this.setState({
-            ...this.state, shouldShowSpinner: false, categories: result.message
+          ...this.state,
+          shouldShowSpinner: false,
+          categories: result.message
         });
-        this.buildCategories(result.message);
-    })
-    .catch(error => {
+      })
+      .catch(error => {
         this.setState({
-            ...this.state, shouldShowSpinner: false
+          ...this.state,
+          shouldShowSpinner: false
         });
-    })
-  } 
-  buildCategories(categories) {
-    const mainDiv = document.querySelector('.categories');
-    for (let index = 0; index < categories.length; index++)
-    {
-        let category = categories[index];
-        let div = document.createElement('div');
-        let header = document.createElement('h3');
-        let anchor = document.createElement('a');
-        let image = document.createElement('img');
-
-        header.innerHTML = category.name;
-        image.src = 'https://github.com/TomerPacific/julOnSale/blob/master/assets/' + category.image + '.png?raw=true';
-        image.setAttribute('class', 'category');
-        anchor.innerHTML = '<img src=' + image.src + ' class="category" alt=' + category.name + ' title=' + category.name + '>';
-        
-        div.appendChild(header);
-        div.appendChild(anchor);
-        div.setAttribute('class', 'container');
-        mainDiv.appendChild(div);
-    }
+      });
   }
+
   render() {
-    return <div className="App">
-    <header className="App-header">
-      What's On Sale @ Jul
-    </header>
-    <DateHeader />
-    <Spinner status = {this.state.shouldShowSpinner}/>
-    <div className="categories">
-      <BrowserRouter>
-        <Route path="categories/:image" componet={Category}/>
-      </BrowserRouter>
-    </div>
-  </div>
+    return (
+      <div className="App">
+        <DateHeader />
+        <Spinner status={this.state.shouldShowSpinner} />
+        <Routes categories={this.state.categories} />
+      </div>
+    );
   }
 }
 
