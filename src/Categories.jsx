84cdd4e97cf61dce69987435c7b7858016d7dc24
unknown <tomerpacific@gmail.com>
2021-02-09 21:25:17 +0200

    julOnSaleReact fixing css rule and adding wrapping div

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -6,18 +6,20 @@ class Categories extends React.Component {
         if (this.props.categories === null) return null;
         return <div className="categories">
             {this.props.categories && this.props.categories.map(category => (
-            <div key={`category-${category.name}`} className={"container"}>
-                <h3>{category.name}</h3>
-                <a href={"/categories" + category.image}>
-                <img src={
-                    "https://github.com/TomerPacific/julOnSale/blob/master/assets/" +
-                    category.image +
-                    ".png?raw=true"
-                    }
-                    alt={`/${category.name}`}
-                    title={category.name}
-                />
-                </a>
+            <div className="container">
+                <div key={`category-${category.name}`} className={"category"}>
+                    <h3>{category.name}</h3>
+                    <a href={"/categories" + category.image}>
+                    <img src={
+                        "https://github.com/TomerPacific/julOnSale/blob/master/assets/" +
+                        category.image +
+                        ".png?raw=true"
+                        }
+                        alt={`/${category.name}`}
+                        title={category.name}
+                    />
+                    </a>
+                </div>
             </div>
         ))}
         </div>
